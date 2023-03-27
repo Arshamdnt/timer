0bce679f182dfbba11bf8a8be4d738c3553f6533
Arshamdnt <arsham6ta@gmail.com>
2023-03-27 15:19:04 +0430

    timer

---
+++ script.js
@@ -0,0 +1,16 @@
+var minute = +prompt("enter minute")
+var second = +prompt("enter second")
+var timer = setInterval(function(){
+
+    if (second === -1) {
+        minute --
+        second = 59
+    }
+    if (minute === 0 && second === 0) {
+        clearInterval(timer)
+    }
+
+    console.log( "timer" +" "+ minute + ":" + second) 
+    second --
+}
+,1000)
