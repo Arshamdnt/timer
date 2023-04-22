f412c97df4f1f0de7bb83dfe156af5cbada536b4
Arshamdnt <arsham6ta@gmail.com>
2023-04-22 14:19:04 +0430

    training

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let para = document.getElementById("paragraph")
-function changeColor() {
-para.style.color = "yellow" 
-}
+let h1Elem = document.getElementById("myh1")
+let btn = document.getElementById("removeing")
+btn.addEventListener("click",function(){
+h1Elem.remove()})
+
